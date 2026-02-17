fed7bbd4b5f421a1bf0dc9d02216f4e55ff09410
jasper <2190320413@qq.com>
2026-02-17 15:35:34 +0800

    mac#vim option.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- option.lua 不需要安装插件就能生效的内置功能。类似VS Code 的 settings.json 文件
+--
+-- 开启绝对行号
+-- vim.opt.number = true
+
+-- 开启相对行号 (行号随着光标跳动)
+-- vim.opt.relativenumber = true
