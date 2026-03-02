37459a735f37a5715a5eb5a18054bc4ceea023c2
jasper <2190320413@qq.com>
2026-03-02 11:45:40 +0800

    java change to lazyextra configlog

--- nvim/lua/plugins/language/java.lua
+++ nvim/lua/plugins/language/java.lua
@@ -1,30 +1,25 @@
-return {
-  "nvim-java/nvim-java",
-  config = function()
-    require("java").setup({
-      -- 插件自身的配置
-      jdtls = {
-        settings = {
-          java = {
-            -- 开启 Maven 自动下载源码和 Javadoc
-            maven = {
-              downloadSources = true,
-              downloadJavadocs = true,
-            },
-            -- 开启 Gradle 自动下载源码
-            eclipse = {
-              downloadSources = true,
-            },
-            -- 允许自动更新构建配置
-            configuration = {
-              updateBuildConfiguration = "interactive",
-            },
-            format = {
-              tabSize = 4, -- 缩进大小为 4
-            },
-          },
-        },
-      },
-    })
-  end,
-}
+local M = {}
+
+function M.setup()
+  local dap = require("dap")
+
+  -- 2. Java 调试配置
+  dap.configurations.java = {
+    {
+      type = "java",
+      request = "launch",
+      name = "Spring Boot (Real-time Log)",
+      -- 关键配置：让日志进入 dap-ui 的 console 窗口
+      console = "internalConsole",
+
+      -- 核心 JVM 参数
+      -- 1. 强制开启彩色日志
+      -- 2. 解决中文乱码
+      -- 3. 提示 Spring 立即刷新输出流
+      vmArgs = "-Dspring.output.ansi.enabled=ALWAYS "
+        .. "-Dfile.encoding=UTF-8 "
+        .. "-Dspring.main.banner-mode=console",
+    },
+  }
+end
+return M
