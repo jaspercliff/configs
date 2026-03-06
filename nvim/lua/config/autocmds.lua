4ca97718fb06ed8fd43f1dba5a41fc141a196fb0
jasper <2190320413@qq.com>
2026-03-06 11:36:14 +0800

    autocmd and copilot

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- java代码保存完自动optimize import
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.java",
+  callback = function()
+    -- 调用 jdtls 的 organize imports 功能
+    -- organize_imports 是 jdtls 扩展协议的一部分
+    require("jdtls").organize_imports()
+  end,
+})
