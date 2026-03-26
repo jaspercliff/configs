51212dd6d84daf9d53d8b0726d4eb5e5d4b18212
jasper <2190320413@qq.com>
2026-03-26 18:30:16 +0800

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,23 +5,25 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- or, changing the font size and color scheme.
-config.font_size = 14
+config.font_size = 16
 config.color_scheme = "AdventureTime"
 
 config.color_scheme = "Catppuccin Mocha"
 
 config.window_background_opacity = 0.85 -- 透明度
 config.macos_window_background_blur = 30 -- Mac 毛玻璃
-config.window_decorations = "RESIZE"
+
+-- config.window_decorations = "RESIZE"
 
 -- 2. 亮色模式下给文字加一点“重量”
 -- 亮色背景下文字容易显得单薄，用 Medium 字重更高级
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
-
+-- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+--
 -- 3. 隐藏繁琐的 Tab 栏，改用更现代的样式
-config.use_fancy_tab_bar = false
-config.tab_max_width = 32
-config.hide_tab_bar_if_only_one_tab = true -- 只有一个标签时自动隐藏，界面极简
+-- config.use_fancy_tab_bar = false
+-- config.tab_max_width = 32
+-- config.hide_tab_bar_if_only_one_tab = true -- 只有一个标签时自动隐藏，界面极简
+
 -- 将 Leader 设为 Space
 config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 1000 }
 
