6706a3d313e636b4f0f270065748f775f4ecd5e9
jasper <2190320413@qq.com>
2026-04-10 15:57:52 +0800

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- or, changing the font size and color scheme.
-config.font_size = 16
+config.font_size = 14
 config.color_scheme = "Catppuccin Mocha"
 
 config.background = {
