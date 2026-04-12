2360a077f957994d4708edd0a618bf65df6f580a
jasper <2190320413@qq.com>
2026-04-12 11:24:37 +0800

    nvim

--- nvim/lua/plugins/confirm.lua
+++ nvim/lua/plugins/confirm.lua
@@ -2,8 +2,14 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      java = { "google_java_format" },
+      java = { "google-java-format" },
       json = { "jq" },
+    },
+    formatters = {
+      ["google-java-format"] = {
+        -- 加上这个参数，缩进就变成 4 空格，和 IDEA 默认一致
+        prepend_args = { "--aosp" },
+      },
     },
   },
 }
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,6 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   opts = {
-    open_mapping = [[<c-\>]], --ctrl+\ terminal float
+    open_mapping = [[<c-\>]],
   },
 }
