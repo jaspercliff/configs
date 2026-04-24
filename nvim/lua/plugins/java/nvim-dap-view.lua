c7629fd67122f59c0cfaa87735eea466d590e4d1
jasper <2190320413@qq.com>
2026-04-24 16:28:57 +0800

    nvim-dap-view

--- nvim/lua/plugins/java/nvim-dap-view.lua
+++ nvim/lua/plugins/java/nvim-dap-view.lua
@@ -1,9 +1,23 @@
 return {
   {
     "igorlfs/nvim-dap-view",
-    -- let the plugin lazy load itself
     lazy = false,
     version = "1.*",
-    opts = {},
+    opts = {
+      winbar = {
+        -- 在这里定义你想要显示的板块及其顺序
+        sections = {
+          "watches",
+          "scopes",
+          "exceptions",
+          "breakpoints",
+          "threads",
+          "repl",
+          "console",
+        },
+        -- 设置默认选中的板块
+        default_section = "scopes",
+      },
+    },
   },
 }
