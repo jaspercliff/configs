3519b80db22546675b1ab6e6e8395ca51d06a9b8
jasper <2190320413@qq.com>
2026-08-20 11:36:00 +0800

    dadbod

--- nvim/lua/plugins/database/dadbod.lua
+++ nvim/lua/plugins/database/dadbod.lua
@@ -21,7 +21,7 @@ return {
       vim.g.db_ui_use_nerd_fonts = 1
 
       vim.g.dbs = {
-        localLearn = "mysql://@127.0.0.1:3307/learn",
+        localLearn = "mysql://root:passwd@127.0.0.1:3306/learn",
         archLearn = "mysql://@arch:3307/learn",
       }
       vim.api.nvim_create_autocmd("FileType", {
