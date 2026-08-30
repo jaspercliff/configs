583c8c9ab673f23154409a81c98b0e128f342fc1
jasper <2190320413@qq.com>
2026-08-30 15:29:37 +0800

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,9 +4,15 @@ local act = wezterm.action
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 5,
+	bottom = 0,
+}
 -- or, changing the font size and color scheme.
 config.font_size = 12
-config.line_height = 1.2
+config.line_height = 1.0
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = true
 -- config.window_decorations = "RESIZE"
@@ -27,7 +33,7 @@ config.background = {
 -- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
 --
 -- 3. 隐藏繁琐的 Tab 栏，改用更现代的样式
--- config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 -- config.tab_max_width = 32
 -- config.hide_tab_bar_if_only_one_tab = true -- 只有一个标签时自动隐藏，界面极简
 
